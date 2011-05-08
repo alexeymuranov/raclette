c2361ab4aaf91362a4c70e04d5644119bfed9f05
Alexey Muranov <muranov@math.univ-toulouse.fr>
2011-05-08 22:29:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raclette::Application.config.secret_token = '94939bf8292ea0f0513cef140b789814889202ecdd65caaa7ae0ee9519d4f5815d8fecfd37259430d48bb8ce61e3e084c7d7274e216eb01d0a1be77aa5cb57b7'
