68dbeefc855cfb73e3298a9daf2d9fa3e45e558c
Alexey Muranov <muranov@math.univ-toulouse.fr>
2011-05-11 13:53:50 +0200

    Basic styles and layout set up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title
+    base_title = 'Raclette'
+    @title.nil? ? base_title : "#{base_title} | #{@title}"
+  end
+
+  def logo
+    image_tag 'raclette.png', :alt => 'raclette'
+  end
 end
