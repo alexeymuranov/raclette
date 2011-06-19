86e2b78656f79fce2f6528d7c95ebe1235bb0011
Alexey Muranov <muranov@math.univ-toulouse.fr>
2011-06-19 11:26:47 +0200

    Simplify and improve views
    Removed separate "show" links from index views and made instead
    usernames and IPs into links.
    Other changes: CSS, translation, layout partials, etc.

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,4 +1,5 @@
 module Admin::UsersHelper
+
   def user_permissions_color_code(user)
     if user.admin?
       '#f00'
@@ -8,6 +9,18 @@ module Admin::UsersHelper
       '#0ff'
     else
       '#0f0'
+    end
+  end
+  
+  def logo_links_to_path(user)
+    if user.admin?
+      admin_admin_tools_overview_path
+    elsif user.manager?
+      root_path
+    elsif user.secretary?
+      root_path
+    else
+      root_path
     end
   end
 end
