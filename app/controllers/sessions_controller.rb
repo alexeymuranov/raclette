5408fee6f1f74cb20106c19feda3cd18f3bceaaf
Alexey Muranov <muranov@math.univ-toulouse.fr>
2011-07-15 17:19:46 +0200

    Add a link to the flash message on login
    First attempt to use HTML links in flash messages from controllers.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class SessionsController < ApplicationController
   skip_before_filter :require_login, :only => [:new, :create]
-
+  
   def new
     @session = ActiveRecord::SessionStore::Session.new
     @title = t('sessions.new.title')
@@ -19,7 +19,16 @@ class SessionsController < ApplicationController
         # clear_return_to  # redundant with reset_session
         reset_session  # security measure
         log_in(user, client_ip)
-        flash[:info] = t('flash_messages.logged_in', :username => user.username)
+
+### XXX NOTE Security Warning:
+###   Need to be sure that 'flash_messages.logged_in' translations
+###   are HTML safe
+        flash[:info] = t('flash_messages.logged_in',
+                         :username => "<a href=\"#{admin_user_path(user)}\">"\
+                                      "#{CGI.escapeHTML(user.username)}"\
+                                      "</a>").html_safe
+###
+
         redirect_to(path || root_url)
       else
         flash.now.alert = t('flash_messages.user_account_deactivated')
