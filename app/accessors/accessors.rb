c76660f7b6042c9b737f4a07d9996815083b59b0
Alexey Muranov <alexey.muranov@gmail.com>
2012-02-13 21:16:30 +0100

    Prepare for mass assignment security in subclasses
    Put commented out "attr_accessible" into Accessors subclasses of some models.

--- app/accessors/accessors.rb
+++ app/accessors/accessors.rb
@@ -22,9 +22,14 @@ module Accessors
     end
   end
 
+  # Active Record:
   class KnownIPResource < Admin::KnownIP
     include ActiveModelUtilities
     include KnowingYourController
+
+    # attr_accessible( :ip,
+                     # :description,
+                     # :as => :admin )
 
     # Redefine class name of the associated model:
     has_many :safe_users, :class_name => :UserResource,
@@ -40,6 +45,23 @@ module Accessors
     include ActiveModelUtilities
     include KnowingYourController
 
+    # attr_accessible( :username,
+                     # :full_name,
+                     # :a_person,
+                     # :email,
+                     # :account_deactivated,
+                     # :admin,
+                     # :manager,
+                     # :secretary,
+                     # :comments,
+                     # :safe_ip_ids,                # association attribute
+                     # :person,                     # association attribute
+                     # :password,                   # virtual attribute
+                     # :password_confirmation,      # virtual attribute
+                     # :new_password,               # virtual attribute
+                     # :new_password_confirmation,  # virtual attribute
+                     # :as => :admin )
+
     has_many :safe_ips, :class_name => :KnownIPResource,
                         :through    => :safe_user_ips,
                         :source     => :known_ip
@@ -53,18 +75,50 @@ module Accessors
     include ActiveModelUtilities
     include KnowingYourController
 
+    # attr_accessible( # :id,
+                     # :event_type,
+                     # :title,
+                     # :locked,
+                     # :lesson,
+                     # :date,
+                     # :start_time,
+                     # :duration_minutes,
+                     # :end_time,
+                     # :location,
+                     # :address_id,
+                     # :weekly,
+                     # :weekly_event_id,
+                     # :supervisors,
+                     # :lesson_supervision_id,
+                     # :entry_fee_tickets,
+                     # :member_entry_fee,
+                     # :couple_entry_fee,
+                     # :common_entry_fee,
+                     # :over,
+                     # :reservations_count,
+                     # :entries_count,
+                     # :member_entries_count,
+                     # :tickets_collected,
+                     # :entry_fees_collected,
+                     # :description
+                   # )
+
     def self.controller_class
       @controller_class || EventsController
     end
   end
 
-  class GuestResource < Guest
-    include ActiveModelUtilities
-  end
-
   class InstructorResource < Instructor
     include ActiveModelUtilities
     include KnowingYourController
+
+    # attr_accessible( # :id,
+                     # :person_id,
+                     # :presentation,
+                     # :photo,
+                     # :employed_from,
+                     # :employed_until
+                   # )
 
     belongs_to :person, :class_name => :PersonResource,
                         :inverse_of => :instructor
@@ -78,6 +132,22 @@ module Accessors
     include ActiveModelUtilities
     include KnowingYourController
 
+    # attr_accessible( :been_member_by,
+                     # :free_tickets_count,
+                     # :last_name,         # delegated attribute
+                     # :first_name,        # delegated attribute
+                     # :name_title,        # delegated attribute
+                     # :nickname_or_other, # delegated attribute
+                     # :birthyear,         # delegated attribute
+                     # :email,             # delegated attribute
+                     # :mobile_phone,      # delegated attribute
+                     # :home_phone,        # delegated attribute
+                     # :work_phone,        # delegated attribute
+                     # :personal_phone,    # delegated attribute
+                     # :primary_address,   # delegated attribute
+                     # :person_attributes, # association attribute
+                     # :as => [:secretary, :manager] )
+
     belongs_to :person, :class_name => :PersonResource,
                         :inverse_of => :member
 
@@ -89,5 +159,17 @@ module Accessors
   class PersonResource < Person
     include ActiveModelUtilities
     include KnowingYourController
+
+    # attr_accessible( :last_name,
+                     # :first_name,
+                     # :name_title,
+                     # :nickname_or_other,
+                     # :email,
+                     # :as => [:secretary, :manager] )
+  end
+
+  # Active Model:
+  class GuestResource < Guest
+    include ActiveModelUtilities
   end
 end
