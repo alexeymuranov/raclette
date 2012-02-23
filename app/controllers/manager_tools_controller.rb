f630fd39d31743af712a1dbab6f28fee27defe7b
Alexey Muranov <alexey.muranov@gmail.com>
2012-02-23 22:29:46 +0100

    Links to new controllers from Manager Tools

--- app/controllers/manager_tools_controller.rb
+++ app/controllers/manager_tools_controller.rb
@@ -4,8 +4,8 @@ class ManagerToolsController < ManagerController
   def overview
 #     @weekly_events = WeeklyEvent.default_order
 #     @memberships = Membership.default_order
-#     @membership_types = MembershipType.default_order
-#     @activity_periods = ActivityPeriod.default_order
+    @membership_types = MembershipType.default_order
+    @activity_periods = ActivityPeriod.default_order
 #     @lesson_supervisions = LessonSupervision.default_order
     @instructors = Instructor.joins(:person)\
                      .with_virtual_attributes(:ordered_full_name, :full_name).default_order
