12c917fe768f12c07fa192c4cbae9d6e0a217733
Alexey Muranov <alexey.muranov@gmail.com>
2012-03-13 23:41:52 +0100

    In overview, show number instead of list if many
    If too many members, show in overview their number instead of the full list.

--- app/controllers/manager_tools_controller.rb
+++ app/controllers/manager_tools_controller.rb
@@ -2,7 +2,13 @@
 
 class ManagerToolsController < ManagerController
   def overview
-    @events = Event.default_order
+    event_count = Event.count
+    if event_count > 50
+      @events = event_count
+    else
+      @events = Event.default_order
+    end
+
     @weekly_events = WeeklyEvent.default_order
 #     @memberships = Membership.default_order
     @ticket_books = TicketBook.default_order
--- app/controllers/secretary_tools_controller.rb
+++ app/controllers/secretary_tools_controller.rb
@@ -2,9 +2,19 @@
 
 class SecretaryToolsController < SecretaryController
   def overview
-    @members = paginate(Member.joins(:person).
-                          with_virtual_attributes(:ordered_full_name,
-                                                  :full_name).default_order)
-    @events = Event.default_order
+    member_count = Member.count
+    if member_count > 50
+      @members = member_count
+    else
+      @members = Member.joins(:person).
+        with_virtual_attributes(:ordered_full_name, :full_name).default_order
+    end
+
+    event_count = Event.count
+    if event_count > 50
+      @events = event_count
+    else
+      @events = Event.default_order
+    end
   end
 end
