7c72f2123846ccee8fe9c3e959cdb7bab26895c2
Alexey Muranov <alexey.muranov@gmail.com>
2012-04-15 02:32:38 +0300

    Improve filter_by_prefix helper

--- app/helpers/filter_markup_helper.rb
+++ app/helpers/filter_markup_helper.rb
@@ -1,9 +1,9 @@
 ## encoding: UTF-8
 
-module FilterMarkupHelper  # WIP
-  def filter_by_prefix(str_attr, prefixes, filtering_values, param_key_nesting=nil, html_options={})  # TODO
+module FilterMarkupHelper
+
+  def filter_by_prefix(str_attr, prefixes, filtering_values, param_key_nesting=nil, html_options={})
     options = { :class => 'filtering_prefixes' }.merge(html_options)
-    # klass = models.klass
     if filtering_values.key?(str_attr)
       current_prefix = filtering_values[str_attr].sub(/\%+\z/, '').mb_chars.upcase.to_s
       current_prefix = prefixes.find { |pref|
@@ -14,22 +14,20 @@ module FilterMarkupHelper  # WIP
     end
     content_tag(:nav, options) do
       form_tag(nil, :method => :get ) do
-        hidden_field_tags_from_param_hash(params) +
-        content_tag(:ul) do
-          html_output = ''.html_safe
-          prefixes.each do |pref|
-            html_output << content_tag(:li, :class => 'filtering_prefix') do
-              if pref == current_prefix
-                pref
-              else
-                content_tag(:button, pref, :name  => "filter[#{str_attr}]",
-                                           :value => pref,
-                                           :type => :submit)
-              end
+        key_name = "filter[#{str_attr}]"
+        saved_params = params.except(key_name).merge( :request_type => 'filter' )
+        hidden_field_tags_from_param_hash(saved_params) <<
+          content_tag(:ul) do
+            prefixes.inject ''.html_safe do |html_output, pref|
+              html_output << content_tag(:li, :class => 'filtering_prefix') {
+                pref == current_prefix ?
+                        pref :
+                        content_tag(:button, pref, :name  => key_name,
+                                                   :value => pref,
+                                                   :type  => 'submit')
+              }
             end
           end
-          html_output.html_safe
-        end
       end
     end
   end
