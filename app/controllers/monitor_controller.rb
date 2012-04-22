02d3fdabc41919f1731f987066d15c151de1351f
Alexey Muranov <alexey.muranov@gmail.com>
2012-04-22 15:02:27 +0200

    Improve MonitorController#overview

--- app/controllers/monitor_controller.rb
+++ app/controllers/monitor_controller.rb
@@ -5,7 +5,8 @@ class MonitorController < ApplicationController
   param_accessible /.+/
 
   def overview
-    @members_count = Member.count
-    @active_members_count = Member.account_active.count
+    # @members_count = Member.count
+    # @active_members_count = Member.account_active.count
+    # @current_members_count = Member.current.count
   end
 end
