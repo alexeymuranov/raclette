5f628b036f2300386e67837ed4e8a0b4d022a38d
Alexey Muranov <alexey.muranov@gmail.com>
2012-05-30 22:36:02 +0200

    Add AbstractHumanizedModel
    AbstractHumanizedModel is a module to be used for localizing and formatting model attribute names and other.

---
+++ app/models/abstract_humanized_model.rb
@@ -0,0 +1,23 @@
+module AbstractHumanizedModel
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def human_column_headers
+      @human_column_headers ||= Hash.new { |hash, key|
+        name = human_attribute_name(key)
+        case attr_db_type(key)
+        when :boolean
+          hash[key] = I18n.t('formats.attribute_name?', :attribute => name)
+        else
+          hash[key] = I18n.t('formats.attribute_name:', :attribute => name)
+        end
+      }
+    end
+
+    def human_column_header(attr)
+      human_column_headers[attr]
+    end
+  end
+end
