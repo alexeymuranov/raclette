c7514df06b756501766117aacd83b89623548d62
Alexey Muranov <alexey.muranov@gmail.com>
2012-07-08 19:33:59 +0200

    Add complex MIME types, as for .csv.zip

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,10 +4,14 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register 'application/xml', :ms_excel_2003_xml
+Mime::Type.register 'application/xml', :ms_excel_2003_xml, [], ['xml.xls', 'xls']
 Mime::Type.register 'application/vnd.ms-excel', :xls
-Mime::Type.register 'application/zip', :zip
+# Mime::Type.register 'application/zip', :zip  # already initialized (standard)
+Mime::Type.register 'application/zip', :ms_excel_2003_xml_zip, [], ['xls.zip', 'zip']
+Mime::Type.register 'application/zip', :csv_zip, [], ['csv.zip', 'zip']
 Mime::Type.register 'application/x-gzip', :gzip
+Mime::Type.register 'application/x-gzip', :ms_excel_2003_xml_gzip, [], ['xls.gzip', 'xls.gz', 'gzip', 'gz']
+Mime::Type.register 'application/x-gzip', :csv_gzip, [], ['csv.gzip', 'csv.gz', 'gzip', 'gz']
 # Mime::Type.register 'application/pdf', :pdf
 # Mime::Type.register 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', :xlsx
 # Mime::Type.register 'application/vnd.oasis.opendocument.spreadsheet', :ods
