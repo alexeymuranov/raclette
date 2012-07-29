4027339032c0284cf61dcbaef2a6d678d576ee4c
Alexey Muranov <alexey.muranov@gmail.com>
2012-07-29 18:53:41 +0200

    Remove CustomResponder stab
    I do not think responders are very usefull.  I prefer to try to create custom renderers.  (Responder uses renderers anyway.)

--- lib/app_responders/responding_ms_excel_2003_xml.rb
+++
@@ -1,2 +0,0 @@
-module RespondingMSExcel2003XML
-end
--- lib/app_responders/responding_paginated.rb
+++
@@ -1,9 +0,0 @@
-module RespondingPaginated
-  def to_html
-    if get? && resource.is_a?(ActiveRecord::Relation)
-      instance_variable_set "@#{controller.controller_name}", resource
-      paginate_resource
-    end
-    super
-  end
-end
