a2a44e7eb1030f6067ccd232380dfa4c584ef8e0
Alexey Muranov <alexey.muranov@gmail.com>
2012-08-06 12:51:24 +0200

    Add some tests for downloading CSV, CSV_ZIP, etc.

--- test/functional/admin/users_controller_test.rb
+++ test/functional/admin/users_controller_test.rb
@@ -14,6 +14,30 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get CSV from index" do
+    session[:user_id] = @user_admin.to_param
+    get :index, :format => 'csv'
+    assert_response :success
+  end
+
+  test "should get MS_EXCEL_2003_XML from index" do
+    session[:user_id] = @user_admin.to_param
+    get :index, :format => 'ms_excel_2003_xml'
+    assert_response :success
+  end
+
+  test "should download CSV_ZIP from index" do
+    session[:user_id] = @user_admin.to_param
+    get :index, :format => 'csv_zip'
+    assert_response :success
+  end
+
+  test "should download MS_EXCEL_2003_XML_ZIP from index" do
+    session[:user_id] = @user_admin.to_param
+    get :index, :format => 'ms_excel_2003_xml_zip'
+    assert_response :success
+  end
+
   test "should get show" do
     session[:user_id] = @user_admin.to_param
     get :show, { :id => @another_user.to_param }
