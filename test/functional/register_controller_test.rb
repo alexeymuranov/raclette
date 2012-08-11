c16be1550919ddc10d22337949c703be4fa08bcc
Alexey Muranov <alexey.muranov@gmail.com>
2012-08-12 00:25:29 +0200

    Add some tests of RegisterController

--- test/functional/register_controller_test.rb
+++ test/functional/register_controller_test.rb
@@ -4,6 +4,11 @@ class RegisterControllerTest < ActionController::TestCase
 
   def setup
     @user_admin = admin_users(:one)
+    @member = members(:one)
+    @event = events(:one)
+    @ticket_book = ticket_books(:one)
+    @membership_type = membership_types(:one)
+    @activity_period = activity_periods(:one)
     # test_log_in(@user_admin, "127.0.0.1")
   end
 
@@ -19,21 +24,50 @@ class RegisterControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # test "should create event_entry" do
+  test "should get new_member_transaction" do
+    session[:user_id] = @user_admin.to_param
+    get :new_member_transaction, { :member_id => @member.to_param }
+    assert_response :success
+  end
+
+  test "should get new_guest_transaction" do
+    session[:user_id] = @user_admin.to_param
+    get :new_guest_transaction
+    assert_response :success
+  end
+
+  test "should create member_entry" do
+    session[:user_id] = @user_admin.to_param
+    post :create_member_entry,
+         { :member_id   => @member.to_param,
+           :event_entry =>
+             { :event_id  => @event.to_param,
+               :person_id => @member.person.to_param } }
+    assert_response :success
+  end
+
+  # test "should create guest_event_entry" do
   #   session[:user_id] = @user_admin.to_param
-  #   post :create_event_entry
+  #   post :create_guest_entry
   #   assert_response :success
   # end
 
-  # test "should create tickets_purchase" do
-  #   session[:user_id] = @user_admin.to_param
-  #   post :create_tickets_purchase
-  #   assert_response :success
-  # end
+  test "should create member_ticket_purchase" do
+    session[:user_id] = @user_admin.to_param
+    post :create_member_ticket_purchase,
+         { :member_id        => @member.to_param,
+           :tickets_purchase =>
+             { :ticket_book_id  => @ticket_book.to_param } }
+    assert_response :success
+  end
 
-  # test "should create membership_purchase" do
-  #   session[:user_id] = @user_admin.to_param
-  #   post :create_membership_purchase
-  #   assert_response :success
-  # end
+  test "should create member_membership_purchase" do
+    session[:user_id] = @user_admin.to_param
+    post :create_member_membership_purchase,
+         { :member_id           => @member.to_param,
+           :membership_purchase =>
+             { :membership_type_id  => @membership_type.to_param,
+               :activity_period_id  => @activity_period.to_param } }
+    assert_response :success
+  end
 end
