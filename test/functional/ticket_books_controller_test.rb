a360f6ad94a4dea65d2129ed6b2446861ec87d2f
Alexey Muranov <alexey.muranov@gmail.com>
2012-09-11 19:39:47 +0200

    Add basic tests for TicketBooksController

--- test/functional/ticket_books_controller_test.rb
+++ test/functional/ticket_books_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class TicketBooksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user_admin = admin_users(:one)
+    @ticket_book = ticket_books(:one)
+    # test_log_in(@user_admin, "127.0.0.1")
+  end
+
+  test "should get index" do
+    session[:user_id] = @user_admin.to_param
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    session[:user_id] = @user_admin.to_param
+    get :show, { :id => @ticket_book.to_param }
+    assert_response :success
+  end
+
+  test "should get new" do
+    session[:user_id] = @user_admin.to_param
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    session[:user_id] = @user_admin.to_param
+    get :edit, { :id => @ticket_book.to_param }
+    assert_response :success
+  end
 end
