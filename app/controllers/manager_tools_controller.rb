50109d1668f3c88e0aa573ba34a4085029c4be3e
Alexey Muranov <alexey.muranov@gmail.com>
2012-09-20 17:01:01 +0200

    Fix memberships display in ManagerToolsController

--- app/controllers/manager_tools_controller.rb
+++ app/controllers/manager_tools_controller.rb
@@ -4,11 +4,12 @@ class ManagerToolsController < ManagerController
 
   def overview
     @weekly_events = Accessors::WeeklyEvent.default_order
-#     @memberships = Accessors::Membership.default_order
+    @memberships = Accessors::Membership.count
     @ticket_books = Accessors::TicketBook.default_order
     @membership_types = Accessors::MembershipType.default_order
     @activity_periods = Accessors::ActivityPeriod.default_order
-    @instructors = Accessors::Instructor.joins(:person)\
-                     .with_pseudo_columns(:ordered_full_name, :full_name).default_order
+    @instructors =
+      Accessors::Instructor.joins(:person).
+        with_pseudo_columns(:ordered_full_name, :full_name).default_order
   end
 end
