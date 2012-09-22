ebc469055471b037916df0a0728477b6796c839e
Alexey Muranov <alexey.muranov@gmail.com>
2012-09-22 19:22:56 +0200

    Allow logout by GET request too

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Raclette::Application.routes.draw do
   get    'login',  :controller => :sessions, :action => :new
   post   'login',  :controller => :sessions, :action => :create
+  get    'logout', :controller => :sessions, :action => :destroy
   delete 'logout', :controller => :sessions, :action => :destroy
 
   get 'monitor/overview'
