6614c696fccaf1b1fbeb8fc51f70f469efc2f645
Alexey Muranov <alexey.muranov@gmail.com>
2012-11-13 20:48:13 +0100

    Fix email format validator regexp

--- lib/app_validations/email_format.rb
+++ lib/app_validations/email_format.rb
@@ -1,6 +1,6 @@
 class EmailFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
-    unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       object.errors[attribute] <<
         (options[:message] || I18n.t('errors.messages.email_format'))
     end
