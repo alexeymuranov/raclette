409cb91fe2038db6d21583e451f88a4225fa93d2
Alexey Muranov <alexey.muranov@gmail.com>
2012-12-29 01:24:24 +0200

    Move most of _attribute_in_description to helper

--- app/helpers/model_presentation_helper.rb
+++ app/helpers/model_presentation_helper.rb
@@ -66,4 +66,42 @@ module ModelPresentationHelper
       :text
     end
   end
+
+  def formatted_attribute_value(value, column_type, readonly = false)
+    return if value.nil?
+    case column_type
+    when :boolean
+      shown_value = boolean_to_yes_no(value)
+      readonly ? content_tag(:b, shown_value) : shown_value
+    when :date, :datetime
+      shown_value = l value, :format => :custom
+      content_tag(:time) do
+        readonly ? content_tag(:b, shown_value) : shown_value
+      end
+    when :time
+      shown_value = l value, :format => :time_of_the_day
+      content_tag(:time) do
+        readonly ? content_tag(:b, shown_value) : shown_value
+      end
+    else
+      readonly ? content_tag(:b, value) : value
+    end
+  end
+
+  def formatted_attribute_in_description(object, attribute, column_type = nil)
+    model = object.class
+    column_type ||= model.column_db_type(attribute)
+    readonly = model.readonly_attributes.include?(attribute.to_s)
+    value = object.public_send(attribute)
+
+    label           = label_from_attribute_name(model, attribute)
+    html_class      = html_class_from_column_type(column_type)
+    formatted_value = formatted_attribute_value(value, column_type, readonly)
+
+    render 'shared/lists/attribute_in_description',
+           :label           => label,
+           :html_class      => html_class,
+           :formatted_value => formatted_value
+
+  end
 end
